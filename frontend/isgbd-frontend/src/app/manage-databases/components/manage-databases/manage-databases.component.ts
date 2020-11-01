9be86f2c17a360e9cddd2043181591ba62d5d3a5
Alexandru Murariu <alexandru.murariu@tss-yonder.com>
2020-11-01 18:39:22 +0200

    Added database filter

--- frontend/isgbd-frontend/src/app/manage-databases/components/manage-databases/manage-databases.component.ts
+++ frontend/isgbd-frontend/src/app/manage-databases/components/manage-databases/manage-databases.component.ts
@@ -1,20 +1,44 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import { filterItems } from 'src/app/shared/helpers/filter-items.helper';
 
 @Component({
   selector: 'app-manage-databases',
   templateUrl: './manage-databases.component.html',
   styleUrls: ['./manage-databases.component.scss']
 })
-export class ManageDatabasesComponent implements OnInit {
+export class ManageDatabasesComponent implements OnInit, OnChanges {
+  filterDatabasesForm: FormGroup;
+  filteredDatabaseList: string[];
+
   @Input() databaseList: string[];
   @Input() databaseIsLoading: boolean;
   @Input() getDatabaseListError: string;
   @Output() selectDatabaseAction: EventEmitter<string> = new EventEmitter<string>();
   @Output() deleteDatabaseAction: EventEmitter<string> = new EventEmitter<string>();
   
-  constructor() { }
+  constructor(private readonly fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.filterDatabasesForm = this.fb.group({
+      searchDatabase: ['']
+    });
+
+    this.searchDatabaseControl.valueChanges.pipe(debounceTime(500))
+    .subscribe((searchString: string) => {
+      this.filteredDatabaseList = this.filterDatabases(searchString);
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.databaseList) {
+      this.filteredDatabaseList = changes.databaseList.currentValue;
+    }
+  }
+
+  get searchDatabaseControl() {
+    return this.filterDatabasesForm.get('searchDatabase');
   }
 
   selectDatabase(databaseName: string) {
@@ -23,5 +47,25 @@ export class ManageDatabasesComponent implements OnInit {
   
   deleteDatabase(databaseName: string) {
     this.deleteDatabaseAction.emit(databaseName);
+  }
+
+  filterDatabases(searchString: string) {
+    if (!searchString) {
+      return this.databaseList;
+    }
+
+    let parsedSearchTerms = searchString.split(' ');
+
+    return this.databaseList.filter((databaseName: string) => {
+      for (let i = 0; i < parsedSearchTerms.length; i++) {
+        if (parsedSearchTerms[i]) {
+          if (!databaseName.toLowerCase().includes(parsedSearchTerms[i].toLowerCase())) {
+            return false; 
+          }
+        }
+      }
+
+      return true;
+    })
   }
 }
--- frontend/isgbd-frontend/src/app/shared/shared.module.ts
+++ frontend/isgbd-frontend/src/app/shared/shared.module.ts
@@ -4,22 +4,32 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     CommonModule,
     MatCardModule,
     MatButtonModule,
     MatIconModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
