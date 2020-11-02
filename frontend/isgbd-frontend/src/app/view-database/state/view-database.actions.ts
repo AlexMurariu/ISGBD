04d1f7189b0751ee2dcb756d036d3c1a327db6e3
Alexandru Murariu <alexandru.murariu@tss-yonder.com>
2020-11-03 01:21:05 +0200

    Added view database actions

--- frontend/isgbd-frontend/src/app/view-database/state/view-database.actions.ts
+++ frontend/isgbd-frontend/src/app/view-database/state/view-database.actions.ts
@@ -1,10 +1,19 @@
 import { Action } from '@ngrx/store';
-import { TableModel } from 'src/app/shared/models';
+import { IndexModel, TableModel } from 'src/app/shared/models';
 
 export const enum ViewDatabaseActionTypes {
     GetDatabaseTables = '[View Database] Get Database Tables',
     GetDatabaseTablesSuccess = '[View Database] Get Database Tables Success',
     GetDatabaseTablesFail = '[View Database] Get Database Tables Fail',
+    CreateTable = '[View Database] Create Table',
+    CreateTableSuccess = '[View Database] Create Table Success',
+    CreateTableFail = '[View Database] Create Table Fail',
+    DropTable = '[View Database] Drop Table',
+    DropTableSuccess = '[View Database] Drop Table Success',
+    DropTableFail = '[View Database] Drop Table Fail',
+    CreateIndex = '[View Database] Create Index',
+    CreateIndexSuccess = '[View Database] Create Index Success',
+    CreateIndexFail = '[View Database] Create Index Fail'
 }
 
 export class GetDatabaseTables implements Action {
@@ -25,6 +34,69 @@ export class GetDatabaseTablesFail implements Action {
     constructor(public payload: string) {}
 }
 
+export class CreateTable implements Action {
+    readonly type = ViewDatabaseActionTypes.CreateTable;
+
+    constructor(public payload: {databaseName: string, table: TableModel}) {}
+}
+
+export class CreateTableSuccess implements Action {
+    readonly type = ViewDatabaseActionTypes.CreateTableSuccess;
+
+    constructor(public payload: TableModel[]) {}
+}
+
+export class CreateTableFail implements Action {
+    readonly type = ViewDatabaseActionTypes.CreateTableFail;
+
+    constructor(public payload: string) {}
+}
+
+export class DropTable implements Action {
+    readonly type = ViewDatabaseActionTypes.DropTable;
+
+    constructor(public payload: {databaseName: string, tableName: string}) {}
+}
+
+export class DropTableSuccess implements Action {
+    readonly type = ViewDatabaseActionTypes.DropTableSuccess;
+
+    constructor(public payload: string) {}
+}
+
+export class DropTableFail implements Action {
+    readonly type = ViewDatabaseActionTypes.DropTableFail;
+
+    constructor(public payload: string) {}
+}
+
+export class CreateIndex implements Action {
+    readonly type = ViewDatabaseActionTypes.CreateIndex;
+
+    constructor(public payload: {databaseName: string, tableName: string, index: IndexModel}) {}
+}
+
+export class CreateIndexSuccess implements Action {
+    readonly type = ViewDatabaseActionTypes.CreateIndexSuccess;
+
+    constructor(public payload: TableModel) {}
+}
+
+export class CreateIndexFail implements Action {
+    readonly type = ViewDatabaseActionTypes.CreateIndexFail;
+
+    constructor(public payload: string) {}
+}
+
 export type ViewDatabaseActions = GetDatabaseTables
     | GetDatabaseTablesSuccess
-    | GetDatabaseTablesFail;
+    | GetDatabaseTablesFail
+    | CreateTable
+    | CreateTableSuccess
+    | CreateTableFail
+    | DropTable
+    | DropTableSuccess
+    | DropTableFail
+    | CreateIndex
+    | CreateIndexSuccess
+    | CreateIndexFail;
